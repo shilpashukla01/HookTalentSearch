601a5f913934707830e068c462b095d6f0775ca0
Shilpa Shukla <shilpa@Shilpas-MacBook-Air.local>
2016-08-27 20:53:41 -0400

    Added Redis cache functionality

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import os
 import format
 from argparse import ArgumentParser
 import logging, sys
-
+import redis
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
+
+DESIRED_CANDIDATES_PER_EMAIL_DIGEST = 5
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -48,23 +50,28 @@ def getGitHubProfiles(locations, languages):
     
     logger.info("Use githubcontributions api to get the number of contributions for each user")
     
-    # TODO: Remove the top 15 when ready
-    for u in matchingUsers[:15]:
+    # TODO: Remove the top 25 when ready
+    for u in matchingUsers[:25]:
         cmd = 'curl -s https://githubcontributions.io/api/user/'+ u.login
         output = subprocess.check_output(cmd, shell=True)
         userActivityDict[u.login] = json.loads(output)['eventCount']
     
     topUsers = sorted(userActivityDict.items(), key=lambda x: x[1], reverse= True)
+
+    logger.info("Emailing top 5 profiles not already in the cache (not already sent before)")
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
     
-    logger.info("Sending information of top 5 matching profiles...")
-    
-    # TODO check if the top 5 of topUsers are already in cache, if yes check next 20 and so on
-    # Send the ones that are not already in the cache (not already sent before)
-    
+    # Run the following when done debugging, to clear the chache
+    # redis-cli flushall
+    count = 0
     for u in topUsers:
-        usr = gh.user(u[0])
-        contributions = u[1]
-        format.format_html(usr, contributions)
+        if count < DESIRED_CANDIDATES_PER_EMAIL_DIGEST:
+            usr = gh.user(u[0])
+            contributions = u[1]
+            if not r.exists(usr.login):
+                format.format_html(usr, contributions)
+                r.set(usr.login, True)
+                count = count + 1
     
     format.save_file()
     
